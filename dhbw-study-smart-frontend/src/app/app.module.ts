4c22805827df966da9ee00c1fc9e6e00beb91751
Yuriko Hoshizawa <hoshizawa.yuriko@gmail.com>
2023-11-19 16:47:52 +0100

    SSW-64 Add Angular Material dependencies

--- dhbw-study-smart-frontend/src/app/app.module.ts
+++ dhbw-study-smart-frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './global/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -12,7 +14,9 @@ import { LoginComponent } from './global/login/login.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]

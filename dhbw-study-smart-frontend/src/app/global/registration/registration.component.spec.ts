8f784694b51fe4d8a8e16bea213d7fbb7d7756ae
Yuriko Hoshizawa <hoshizawa.yuriko@gmail.com>
2023-11-30 10:14:23 +0100

    SSW-68 Fix test case for registration from

--- dhbw-study-smart-frontend/src/app/global/registration/registration.component.spec.ts
+++ dhbw-study-smart-frontend/src/app/global/registration/registration.component.spec.ts
@@ -5,6 +5,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 import { RegistrationComponent } from './registration.component';
 
@@ -20,7 +22,9 @@ describe('RegistrationComponent', () => {
                 BrowserAnimationsModule,
                 MatFormFieldModule,
                 MatInputModule,
-                MatCardModule
+                MatCardModule,
+                MatSelectModule,
+                MatOptionModule
             ],
             declarations: [RegistrationComponent]
         });

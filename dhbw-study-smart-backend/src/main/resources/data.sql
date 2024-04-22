88fc218438d563ab38fcc678fa84da0607fd3a2c
Yuriko Hoshizawa <hoshizawa.yuriko@gmail.com>
2024-04-22 16:10:19 +0200

    SSW-18 Add additional documents for downloading

--- dhbw-study-smart-backend/src/main/resources/data.sql
+++ dhbw-study-smart-backend/src/main/resources/data.sql
@@ -25,3 +25,5 @@ INSERT INTO LECTURE (lecture_id, name) VALUES (1, 'Theoretische Informatik I');
 
 -- Documents
 INSERT INTO DOCUMENT (document_id, path, filename, lecture_id, semester_id) VALUES (1, 'static\TINF-Praxis-Bestaetigung.docx', 'TINF-Praxis-Bestaetigung.docx', 1, 1);
+INSERT INTO DOCUMENT (document_id, path, filename, lecture_id, semester_id) VALUES (2, 'static\INF_Konzept_digit.pdf', 'INF_Konzept_digit.pdf', 1, 1);
+INSERT INTO DOCUMENT (document_id, path, filename, lecture_id, semester_id) VALUES (3, 'static\TINF-Praxis-Reflexion.docx', 'TINF-Praxis-Reflexion.docx', 1, 1);

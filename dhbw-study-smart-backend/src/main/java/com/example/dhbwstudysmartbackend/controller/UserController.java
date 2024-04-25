b1091a8533371059d33718a93a45347c4ad4dd89
Amjad Alnakshbandi <113834304+amjadalnakshbandi@users.noreply.github.com>
2024-04-25 12:12:20 +0200

    SSW-80 - add logout button

--- dhbw-study-smart-backend/src/main/java/com/example/dhbwstudysmartbackend/controller/UserController.java
+++ dhbw-study-smart-backend/src/main/java/com/example/dhbwstudysmartbackend/controller/UserController.java
@@ -5,7 +5,10 @@ import com.example.dhbwstudysmartbackend.entity.userDTOs.LoginUserDTO;
 import com.example.dhbwstudysmartbackend.entity.userDTOs.RegistrationUserDTO;
 import com.example.dhbwstudysmartbackend.service.UsersService;
 import com.example.dhbwstudysmartbackend.service.serviceImpl.UsersServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,11 +27,20 @@ public class UserController {
     @GetMapping("/verify")
     @ResponseBody
     public LoginUserDTO verifyUser(@RequestParam(name = "email") String email, @RequestParam(name = "password") String password) {
-        return service.verifyUser(email,password);
+        return service.verifyUser(email, password);
     }
 
     @PostMapping("/register")
     public Users registerUser(@RequestBody RegistrationUserDTO registrationUserDTO) {
         return usersService.registerUser(registrationUserDTO);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // Sitzung beenden
+        }
+        return ResponseEntity.ok("User logged out successfully");
     }
 }
--- dhbw-study-smart-frontend/src/app/global/main-frame/main-frame-header/main-frame-header.component.ts
+++ dhbw-study-smart-frontend/src/app/global/main-frame/main-frame-header/main-frame-header.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
-import { MainFrameService } from '../main-frame.service';
+import {Component, Injectable} from '@angular/core';
+import {MainFrameService} from '../main-frame.service';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import Swal from "sweetalert2";
 
 @Component({
     selector: 'app-main-frame-header',
     templateUrl: './main-frame-header.component.html',
     styleUrls: ['./main-frame-header.component.scss']
 })
+@Injectable({
+    providedIn: 'root'
+})
 export class MainFrameHeaderComponent {
 
-    constructor(private mainFrameService: MainFrameService) {}
+    constructor(private mainFrameService: MainFrameService, private http: HttpClient, private router: Router) {
+    }
 
     // Trigger event when the nav toggle button is clicked
     public triggerNavToggle() {
         this.mainFrameService.emitNavToggle('Nav Toggle in Header triggered');
+    }
+
+    logout() {
+        this.http.post('http://localhost:8080/user/logout', {}, {responseType: 'text'}) // Beachten Sie den responseType
+            .subscribe(
+                () => {
+                    console.log('Logout successful');
+                    localStorage.removeItem('isLoggedIn');
+                    localStorage.setItem('isLoggedIn', 'false');
+                    localStorage.removeItem('userId');
+                    localStorage.setItem('userId', String(null));
+                    this.router.navigate(['/login']); // Umleitung zur Login-Seite
+                    Swal.fire({
+                        title: "Erfolgreich abgemeldet!",
+                        text: "Sie Wurden erfolgreich abgemeldet.",
+                        icon: "success"
+                    });
+                },
+                error => {
+                    Swal.fire({
+                        title: "Abmeldung fehlgeschlagen!",
+                        text: "Wir könnten Sie nicht erfolgreich abmelden, bitte versuchen Sie erneuert.",
+                        icon: "error"
+                    });
+                    console.error('Logout failed', error);
+                }
+            );
     }
 }

65c18ef09f02dabd26ab1f0692c846ee2ebe851e
Yuriko Hoshizawa <hoshizawa.yuriko@gmail.com>
2024-05-06 16:58:22 +0200

    SSW-20 Fix tests for calendar component and calendar service

--- dhbw-study-smart-frontend/src/app/features/calendar/calendar.component.spec.ts
+++ dhbw-study-smart-frontend/src/app/features/calendar/calendar.component.spec.ts
@@ -1,6 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 import {CalendarComponent} from './calendar.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {FormsModule} from '@angular/forms';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 
 describe('CalendarComponent', () => {
     let component: CalendarComponent;
@@ -9,6 +14,12 @@ describe('CalendarComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [CalendarComponent],
+            imports: [
+                HttpClientTestingModule,
+                MatButtonToggleModule,
+                FormsModule,
+                CalendarModule.forRoot({provide: DateAdapter, useFactory: adapterFactory}),
+            ],
         });
         fixture = TestBed.createComponent(CalendarComponent);
         component = fixture.componentInstance;
--- dhbw-study-smart-frontend/src/app/features/calendar/calendar.service.spec.ts
+++ dhbw-study-smart-frontend/src/app/features/calendar/calendar.service.spec.ts
@@ -1,4 +1,5 @@
 import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 import {CalendarService} from './calendar.service';
 
@@ -6,7 +7,10 @@ describe('CalendarService', () => {
     let service: CalendarService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [CalendarService],
+        });
         service = TestBed.inject(CalendarService);
     });
 

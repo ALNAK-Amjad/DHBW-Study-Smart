7b03b358f4f69f0158471ff08e0f342531b96bdd
Amjad Alnakshbandi <113834304+amjadalnakshbandi@users.noreply.github.com>
2024-05-31 19:24:17 +0200

    SSW-17 - added all lectures for the fifth and sixth semesters

--- dhbw-study-smart-backend/src/main/resources/data.sql
+++ dhbw-study-smart-backend/src/main/resources/data.sql
@@ -61,38 +61,115 @@ INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (29, 'Workflow', 4);
 
 
 -- Fünfte Semester
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (30, 'Software Engineering II', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (31, 'Datenbanken II', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (32, 'Consulting, technischer Vetrieb und Recht', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (33, 'Funknetze 1', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (34, 'Funknetze 2', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (35, 'Weitverkehrsnetze 1', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (36, 'Weitverkehrsnetze 2', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (37, 'Computergraphik', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (38, 'Digitale Bildverarbeitung', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (39, 'Interaktive Systeme', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (40, 'Grundlagen der künstlichen Intelligenz', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (41, 'Digitale Bildverarbeitung', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (42, 'Grundlagen der künstlichen Intelligenz', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (43, 'Angewandtes Projektmanagement', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (44, 'eBusiness', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (45, 'Robotik 1', 5);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (46, 'Robotik 2', 5);
+
 
 -- Sechste Semester
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (47, 'IT-Sicherheit', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (48, 'Softwarequalität und Verteilte Systeme', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (49, 'ERP-Systeme', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (50, 'Evolutionäre Algorithmen', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (51, 'Robotik', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (52, 'Web-Services', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (53, 'Gamification', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (54, 'High Performance Computing', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (55, 'Psychologische Grundlagen für Informatiker', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (56, 'Games und Gaming', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (57, 'Kryptographische Verfahren', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (58, 'Ethik für Informatiker', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (59, 'Erklärbare Künstliche Intelligenz', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (60, 'Digitale Audiosignalverarbeitung', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (61, 'Digitale Forensik', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (62, 'Funktionale Sicherheit', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (63, 'Einführung in Rust', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (64, 'Software-Ergonomie und Usability', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (65, 'Machine Learning (supervised)', 6);
+INSERT INTO LECTURE (lecture_id, name, semester_id) VALUES (66, 'Machine Learning (unsupervised)', 6);
 
 
 
+-- Vorlesung gruppe erstellen
 INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (1, 'Mathematik 1');
 INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (2, 'Schlüsselqualifikationen 1');
 INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (3, 'Schlüsselqualifikationen 2');
 INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (4, 'Technische Informatik 2');
 INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (5, 'Wahlmodul Informatik Karlsruhe');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (6, 'Kommunikations- und Netztechnik III');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (7, 'Computergraphik und Bildverarbeitung');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (8, 'Künstliche Intelligenz und Interaktive Systeme');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (9, 'Künstliche Intelligenz und Bildverarbeitung');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (10, 'E-Business');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (11, 'Robotik');
+INSERT INTO LECTURE_GROUP(lecture_group_id, name) VALUES (12, 'Wahlmodul Informatik 3. SJ KA');
 
 
 
+-- Vorlesungen zur der Gruppen zu Ordnen
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (1, 3 , 1);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (2, 7 , 1);
-
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (3, 10 , 2);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (4, 11 , 2);
-
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (5, 12 , 3);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (6, 13 , 3);
-
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (7, 19 , 4);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (8, 20 , 4);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (9, 21 , 4);
-
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (10, 24 , 5);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (11, 25 , 5);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (12, 26 , 5);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (13, 27 , 5);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (14, 28 , 5);
 INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (15, 29 , 5);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (16, 33, 6);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (17, 34, 6);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (18, 35, 6);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (19, 36, 6);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (20, 37, 7);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (21, 38, 7);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (22, 39, 8);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (23, 40, 8);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (24, 41, 9);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (25, 42, 9);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (26, 43, 10);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (27, 44, 10);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (28, 45, 11);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id ) VALUES (29, 46, 11);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (32, 49, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (33, 50, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (34, 51, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (35, 52, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (36, 53, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (37, 54, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (38, 55, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (39, 56, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (40, 57, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (41, 58, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (42, 59, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (43, 60, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (44, 61, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (45, 62, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (46, 63, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (47, 64, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (48, 65, 12);
+INSERT INTO CON_LECTURE_TO_GROUP(con_lecture_to_group_id, lecture_id, lecture_group_id) VALUES (49, 66, 12);
+
+
 
 
 

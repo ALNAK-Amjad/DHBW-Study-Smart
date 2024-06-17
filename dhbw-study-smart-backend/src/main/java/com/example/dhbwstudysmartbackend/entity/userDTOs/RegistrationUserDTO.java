9fc04b2e228dc4724e1203f57b79246a8139b3de
okramDE <ole@kram.de>
2024-06-17 10:21:09 +0200

    Added tests for UserService

--- dhbw-study-smart-backend/src/main/java/com/example/dhbwstudysmartbackend/entity/userDTOs/RegistrationUserDTO.java
+++ dhbw-study-smart-backend/src/main/java/com/example/dhbwstudysmartbackend/entity/userDTOs/RegistrationUserDTO.java
@@ -2,10 +2,12 @@ package com.example.dhbwstudysmartbackend.entity.userDTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationUserDTO {
     private String firstName;
